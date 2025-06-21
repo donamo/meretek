10b18969f1de0b41de374787b6810e8bdb9acb16
Szabo Ambrus <szabo.ambrus@mbhbank.hu>
2025-06-21 08:47:53 +0200

    Initial Commit

---
+++ meretek/meretekApp.swift
@@ -0,0 +1,17 @@
+//
+//  meretekApp.swift
+//  meretek
+//
+//  Created by Szabo Ambrus on 2025. 06. 21..
+//
+
+import SwiftUI
+
+@main
+struct meretekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ meretekTests/meretekTests.swift
@@ -0,0 +1,16 @@
+//
+//  meretekTests.swift
+//  meretekTests
+//
+//  Created by Szabo Ambrus on 2025. 06. 21..
+//
+
+import Testing
+
+struct meretekTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
